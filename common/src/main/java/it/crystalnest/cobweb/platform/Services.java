7b55ad5eadccadf6b6baefa2479a5312831ffee5
Crystal Spider <riccardo.chimisso@gmail.com>
2024-05-08 14:10:09 +0200

    Fix NPE when using config with NeoForge.

--- common/src/main/java/it/crystalnest/cobweb/platform/Services.java
+++ common/src/main/java/it/crystalnest/cobweb/platform/Services.java
@@ -1,6 +1,7 @@
 package it.crystalnest.cobweb.platform;
 
 import it.crystalnest.cobweb.Constants;
+import it.crystalnest.cobweb.platform.model.Platform;
 import it.crystalnest.cobweb.platform.services.ConfigHelper;
 import it.crystalnest.cobweb.platform.services.PlatformHelper;
 import it.crystalnest.cobweb.platform.services.ToolTiersHelper;
@@ -27,7 +28,7 @@ public final class Services {
    * Provides registration for configuration specs.
    */
   @Nullable
-  public static final ConfigHelper CONFIG = PLATFORM.isModLoaded("forgeconfigapiport") ? load(ConfigHelper.class) : null;
+  public static final ConfigHelper CONFIG = PLATFORM.getPlatformName() == Platform.NEOFORGE || PLATFORM.isModLoaded("forgeconfigapiport") ? load(ConfigHelper.class) : null;
 
   private Services() {}
 
