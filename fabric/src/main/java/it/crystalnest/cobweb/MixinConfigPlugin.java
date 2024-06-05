6568aebd813861cbfddcf808ea05efe9eba1a79d
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-05 11:19:26 +0200

    Javadoc.

--- fabric/src/main/java/it/crystalnest/cobweb/MixinConfigPlugin.java
+++ fabric/src/main/java/it/crystalnest/cobweb/MixinConfigPlugin.java
@@ -2,6 +2,7 @@ package it.crystalnest.cobweb;
 
 import com.bawnorton.mixinsquared.MixinSquaredBootstrap;
 import it.crystalnest.cobweb.platform.Services;
+import org.jetbrains.annotations.ApiStatus;
 import org.objectweb.asm.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
 import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
@@ -9,6 +10,10 @@ import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * Mixin Squared plugin configuration.
+ */
+@ApiStatus.Internal
 public class MixinConfigPlugin implements IMixinConfigPlugin {
   @Override
   public void onLoad(String mixinPackage) {
