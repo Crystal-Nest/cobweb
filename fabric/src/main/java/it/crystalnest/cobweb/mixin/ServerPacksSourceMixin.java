7f7c4a235cdb59023494061be148e5c6fd3e2556
Crystal Spider <riccardo.chimisso@gmail.com>
2024-06-16 16:13:47 +0200

    Fix dynamic resource packs.

--- fabric/src/main/java/it/crystalnest/cobweb/mixin/ServerPacksSourceMixin.java
+++ fabric/src/main/java/it/crystalnest/cobweb/mixin/ServerPacksSourceMixin.java
@@ -5,7 +5,6 @@ import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraft.server.packs.repository.RepositorySource;
 import net.minecraft.server.packs.repository.ServerPacksSource;
-import net.minecraft.world.level.validation.DirectoryValidator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
@@ -18,13 +17,13 @@ import java.nio.file.Path;
 @Mixin(ServerPacksSource.class)
 public abstract class ServerPacksSourceMixin {
   /**
-   * Modifies the {@link RepositorySource} argument for the {@link PackRepository} constructor in the method {@link ServerPacksSource#createPackRepository(Path, DirectoryValidator)}.<br />
+   * Modifies the {@link RepositorySource} argument for the {@link PackRepository} constructor in the method {@link ServerPacksSource#createPackRepository(Path)}.<br />
    * Adds all the registered dynamic data packs to the {@link PackRepository#sources}.
    *
    * @param sources original {@link RepositorySource}s.
    * @return modified {@link RepositorySource}s.
    */
-  @ModifyArg(method = "createPackRepository(Ljava/nio/file/Path;Lnet/minecraft/world/level/validation/DirectoryValidator;)Lnet/minecraft/server/packs/repository/PackRepository;", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;<init>([Lnet/minecraft/server/packs/repository/RepositorySource;)V"))
+  @ModifyArg(method = "createPackRepository(Ljava/nio/file/Path;)Lnet/minecraft/server/packs/repository/PackRepository;", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/packs/repository/PackRepository;<init>([Lnet/minecraft/server/packs/repository/RepositorySource;)V"))
   private static RepositorySource[] onCreatePackRepository(RepositorySource[] sources) {
     RepositorySource[] providers = new RepositorySource[sources.length + FabricRegistryHelper.DYNAMIC_DATA_PACKS.size()];
     System.arraycopy(sources, 0, providers, 0, sources.length);
