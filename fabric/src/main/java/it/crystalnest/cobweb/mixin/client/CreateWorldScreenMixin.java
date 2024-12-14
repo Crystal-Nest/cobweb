3e43a9bf743408936f77f391f46ed461027ad54c
Crystal Spider <riccardo.chimisso@gmail.com>
2024-12-14 21:44:42 +0100

    Fix Mixin.

--- fabric/src/main/java/it/crystalnest/cobweb/mixin/client/CreateWorldScreenMixin.java
+++ fabric/src/main/java/it/crystalnest/cobweb/mixin/client/CreateWorldScreenMixin.java
@@ -3,10 +3,7 @@ package it.crystalnest.cobweb.mixin.client;
 import it.crystalnest.cobweb.platform.FabricRegistryHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.client.gui.screens.worldselection.CreateWorldCallback;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
-import net.minecraft.client.gui.screens.worldselection.WorldCreationContextMapper;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.packs.repository.PackRepository;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,14 +15,14 @@ import org.spongepowered.asm.mixin.injection.ModifyVariable;
 @Mixin(CreateWorldScreen.class)
 public abstract class CreateWorldScreenMixin {
   /**
-   * Modifies the {@link PackRepository} variable in the method {@link CreateWorldScreen#openCreateWorldScreen(Minecraft, Screen, Function, WorldCreationContextMapper, ResourceKey, CreateWorldCallback)}.<br>
+   * Modifies the {@link PackRepository} variable in the method {@link CreateWorldScreen#openFresh(Minecraft, Screen)} )}.<br>
    * Registers all dynamic and static data packs to the original repository.
    *
    * @param repository original {@link PackRepository}.
    * @return updated repository.
    */
   @ModifyVariable(method = "openFresh", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/worldselection/CreateWorldScreen;createDefaultLoadConfig(Lnet/minecraft/server/packs/repository/PackRepository;Lnet/minecraft/world/level/WorldDataConfiguration;)Lnet/minecraft/server/WorldLoader$InitConfig;"))
-  private static PackRepository onOpenCreateWorldScreen(PackRepository repository) {
+  private static PackRepository onOpenFresh(PackRepository repository) {
     return FabricRegistryHelper.registerDataPacks(repository);
   }
 }
