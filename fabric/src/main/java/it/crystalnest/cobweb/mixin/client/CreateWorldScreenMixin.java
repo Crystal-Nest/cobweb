b2f7d550e5d37c54e3d775cb5b511cf84f7f5c69
Crystal Spider <riccardo.chimisso@gmail.com>
2026-04-01 12:22:13 +0200

    Fix Javadoc.

--- fabric/src/main/java/it/crystalnest/cobweb/mixin/client/CreateWorldScreenMixin.java
+++ fabric/src/main/java/it/crystalnest/cobweb/mixin/client/CreateWorldScreenMixin.java
@@ -2,7 +2,6 @@ package it.crystalnest.cobweb.mixin.client;
 
 import it.crystalnest.cobweb.platform.FabricRegistryHelper;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldCallback;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import net.minecraft.client.gui.screens.worldselection.WorldCreationContextMapper;
@@ -20,14 +19,14 @@ import java.util.function.Function;
 @Mixin(CreateWorldScreen.class)
 public abstract class CreateWorldScreenMixin {
   /**
-   * Modifies the {@link PackRepository} variable in the method {@link CreateWorldScreen#openCreateWorldScreen(Minecraft, Screen, Function, WorldCreationContextMapper, ResourceKey, CreateWorldCallback)}.<br>
+   * Modifies the {@link PackRepository} variable in the method {@link CreateWorldScreen#openCreateWorldScreen(Minecraft, Runnable, Function, WorldCreationContextMapper, ResourceKey, CreateWorldCallback)}.<br>
    * Registers all dynamic and static data packs to the original repository.
    *
-   * @param repository original {@link PackRepository}.
+   * @param vanillaOnlyPackRepository original {@link PackRepository}.
    * @return updated repository.
    */
   @ModifyVariable(method = "openCreateWorldScreen", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/worldselection/CreateWorldScreen;createDefaultLoadConfig(Lnet/minecraft/server/packs/repository/PackRepository;Lnet/minecraft/world/level/WorldDataConfiguration;)Lnet/minecraft/server/WorldLoader$InitConfig;"))
-  private static PackRepository onOpenCreateWorldScreen(PackRepository repository) {
-    return FabricRegistryHelper.registerDataPacks(repository);
+  private static PackRepository onOpenCreateWorldScreen(PackRepository vanillaOnlyPackRepository) {
+    return FabricRegistryHelper.registerDataPacks(vanillaOnlyPackRepository);
   }
 }
